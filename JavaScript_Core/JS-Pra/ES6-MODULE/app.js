e30396f56ae9bbb75762aaf92820fc525a4d8239
iamtheshru <s.j.patel.3110@gmail.com>
2023-08-23 17:09:19 +0530

    js

---
+++ JavaScript_Core/JS-Pra/ES6-MODULE/app.js
@@ -0,0 +1,8 @@
+import { fname } from './module/name.js'
+import { age } from './module/age.js'
+import { person } from './module/person.js'
+
+console.log(fname, age);
+const person1 = new person("zeel", "patel", 25)
+person1.info();
+console.log(person);
